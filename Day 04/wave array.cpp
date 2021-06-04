d2e77015776fe4eff16f6f10143822f2c3620c62
jtomar24 <75982877+jtomar24@users.noreply.github.com>
2021-06-04 23:59:32 +0530

    GFG

--- Day 04/wave array.cpp
+++ Day 04/wave array.cpp
@@ -1,4 +1,6 @@
-// { Driver Code Starts
+// Given a sorted array arr[] of distinct integers.
+//Sort the array into a wave-like array and return it. 
+//In other words, arrange the elements into a sequence such that a1 >= a2 <= a3 >= a4 <= a5..... (considering the increasing lexicographical order).
 #include<bits/stdc++.h>
 using namespace std;
 
