aeff365cb86606579c109b9adfef8d06fef14d99
jtomar24 <75982877+jtomar24@users.noreply.github.com>
2021-06-06 22:56:59 +0530

    GFG

---
+++ Day 06/Largest Sum Contiguous Subarray.cpp
@@ -0,0 +1,72 @@
+// Largest Sum Contiguous Subarray
+
+#include <bits/stdc++.h>
+using namespace std;
+int maxSubArraySum(int a[], int size)
+{
+	int max_so_far = INT_MIN, max_ending_here = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		max_ending_here = max_ending_here + a[i];
+		if (max_so_far < max_ending_here)
+			max_so_far = max_ending_here;
+
+		if (max_ending_here < 0)
+			max_ending_here = 0;
+	}
+	return max_so_far;
+}
+
+
+int main()
+{
+	int a[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+	int n = sizeof(a)/sizeof(a[0]);
+	int max_sum = maxSubArraySum(a, n);
+	cout << "Maximum contiguous sum is " << max_sum;
+	return 0;
+}
+
+/*
+ Lets take the example:
+    {-2, -3, 4, -1, -2, 1, 5, -3}
+
+    max_so_far = max_ending_here = 0
+
+    for i=0,  a[0] =  -2
+    max_ending_here = max_ending_here + (-2)
+    Set max_ending_here = 0 because max_ending_here < 0
+
+    for i=1,  a[1] =  -3
+    max_ending_here = max_ending_here + (-3)
+    Set max_ending_here = 0 because max_ending_here < 0
+
+    for i=2,  a[2] =  4
+    max_ending_here = max_ending_here + (4)
+    max_ending_here = 4
+    max_so_far is updated to 4 because max_ending_here greater 
+    than max_so_far which was 0 till now
+
+    for i=3,  a[3] =  -1
+    max_ending_here = max_ending_here + (-1)
+    max_ending_here = 3
+
+    for i=4,  a[4] =  -2
+    max_ending_here = max_ending_here + (-2)
+    max_ending_here = 1
+
+    for i=5,  a[5] =  1
+    max_ending_here = max_ending_here + (1)
+    max_ending_here = 2
+
+    for i=6,  a[6] =  5
+    max_ending_here = max_ending_here + (5)
+    max_ending_here = 7
+    max_so_far is updated to 7 because max_ending_here is 
+    greater than max_so_far
+
+    for i=7,  a[7] =  -3
+    max_ending_here = max_ending_here + (-3)
+    max_ending_here = 4
+    */
