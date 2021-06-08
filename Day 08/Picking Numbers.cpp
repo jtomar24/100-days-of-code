a8d42d3e87e117c5d31e05301f7628cfd890525d
jtomar24 <75982877+jtomar24@users.noreply.github.com>
2021-06-08 23:56:41 +0530

    HackerRank
    https://www.hackerrank.com/challenges/picking-numbers/problem

---
+++ Day 08/Picking Numbers.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//int A[105];
+
+int main(){
+    int n;
+    cin >> n;
+    
+    int a[n];    
+    for(int i = 0;i < n;i++){
+      // int x;
+        cin >> i;
+        
+      //  A[x]++;
+    }
+    
+    int pos=0,max=0,count=1;
+    sort(a,a+n);
+    for(int i=1; i<=n; i++)
+        ans = max(aA[i]+A[i-1]);
+    
+    cout << ans << endl;
+    
+    return 0;
+}
