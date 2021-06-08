ac5162a111868e0160ff4c37e67c5ef05182b4b2
jtomar24 <75982877+jtomar24@users.noreply.github.com>
2021-06-08 23:12:27 +0530

    HackerRank
    https://www.hackerrank.com/challenges/runningtime/problem

---
+++ Day 08/Running Time of Algorithms.cpp
@@ -0,0 +1,23 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int count = 0; 
+    for (int i = 0; i < n - 1; i++) 
+        for (int j = i + 1; j < n; j++) 
+            if (arr[i] > arr[j]) 
+                count++; 
+  
+    cout<<count;
+    return 0; 
+}
