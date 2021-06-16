2ae90e2fa060917a76231fae00c7818d7e19e5de
jtomar24 <75982877+jtomar24@users.noreply.github.com>
2021-06-16 23:52:41 +0530

    GFG
    https://www.geeksforgeeks.org/implement-two-stacks-in-an-array/

---
+++ Day 16/Implement two stacks in an array.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+//#define n=10
+
+class twoStacks
+{
+    int* arr;
+    int size;
+    int top1,top2;
+    
+    
+    public:
+    twoStacks(int n)
+    {
+        size=n;
+        arr=new int[n];
+        top1=-1;
+        top2=size;
+    }
+    
+    void push1(int x)
+    {
+        if(top1<top2-1)
+        {
+            top1++;
+            arr[top1]=x;
+        }
+        else
+        {
+            cout << "Stack Overflow";
+        }
+    }
+    
+    void push2(int x)
+    {
+        if(top1<top2-1)
+        {
+            top2--;
+            arr[top2]=x;
+        }
+        else
+        {
+            cout << "Stack Overflow";
+        }
+    }
+    
+    int pop1()
+    {
+       if(top1>=0)
+       {
+           int x=arr[top1];
+           top1--;
+           return x;
+       }
+       else
+       {
+           cout << "Stack UnderFlow";;
+       }
+    }
+    
+    int pop2()
+    {
+       if(top2<size)
+       {
+           int x=arr[top2];
+           top2++;
+           return x;
+       }
+       else
+       {
+           cout << "Stack UnderFlow";;
+       }
+    }
+};
+
+int main() {
+	twoStacks ts(5);
+    ts.push1(5);
+    ts.push2(10);
+    ts.push2(15);
+    ts.push1(11);
+    ts.push2(7);
+    cout << "Popped element from stack1 is "
+         << ts.pop1();
+    ts.push2(40);
+    cout << "\nPopped element from stack2 is "
+         << ts.pop2();
+	
+	return 0;
+}
