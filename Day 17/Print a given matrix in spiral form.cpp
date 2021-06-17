0ede704359bae34e7815ab26aa5ca84eb80ebbce
jtomar24 <75982877+jtomar24@users.noreply.github.com>
2021-06-18 00:04:15 +0530

    GFG
    https://www.geeksforgeeks.org/print-a-given-matrix-in-spiral-form/

--- Day 17/Print a given matrix in spiral form.cpp
+++ Day 17/Print a given matrix in spiral form.cpp
@@ -1,65 +1,54 @@
-// C++ Program to print a matrix spirally
-
 #include <bits/stdc++.h>
 using namespace std;
 #define R 3
 #define C 6
 
-void spiralPrint(int m, int n, int a[R][C])
+void spiralPrint(int m,int n,int a[R][C])
 {
-	int i, k = 0, l = 0;
-
-	/* k - starting row index
-		m - ending row index
-		l - starting column index
-		n - ending column index
-		i - iterator
-	*/
-
-	while (k < m && l < n) {
-		/* Print the first row from
-			the remaining rows */
-		for (i = l; i < n; ++i) {
-			cout << a[k][i] << " ";
-		}
-		k++;
-
-		/* Print the last column
-		from the remaining columns */
-		for (i = k; i < m; ++i) {
-			cout << a[i][n - 1] << " ";
-		}
-		n--;
-
-		/* Print the last row from
-				the remaining rows */
-		if (k < m) {
-			for (i = n - 1; i >= l; --i) {
-				cout << a[m - 1][i] << " ";
-			}
-			m--;
-		}
-
-		/* Print the first column from
-				the remaining columns */
-		if (l < n) {
-			for (i = m - 1; i >= k; --i) {
-				cout << a[i][l] << " ";
-			}
-			l++;
-		}
-	}
+    int i,first_row=0,first_col=0;
+    int last_row=m-1,last_col=n-1;
+    
+    while(first_row<=last_row && first_col<=last_col)
+    {
+        for(i=first_col;i<=last_col;i++)
+        {
+            cout<< a[first_row][i]<<" ";
+        }
+        first_row++;
+        
+        for(i=first_row;i<=last_row;i++)
+        {
+            cout<<a[i][last_col]<<" ";
+        }
+        last_col--;
+        
+      
+        
+        if(first_row<=last_row)
+        {
+            for(i=last_col;i>=first_col;i--)
+            {
+                cout<<a[last_row][i]<<" ";
+            }
+            last_row--;
+        }
+        
+        if(first_col<=last_col)
+        {
+            for(i=last_row;i>=first_row;i--)
+            {
+                cout<<a[i][first_col]<<" ";
+            }
+            last_col--;
+        }
+}
 }
 
-
-int main()
-{
-	int a[R][C] = { { 1, 2, 3, 4, 5, 6 },
-					{ 7, 8, 9, 10, 11, 12 },
-					{ 13, 14, 15, 16, 17, 18 } };
-	
-
-	spiralPrint(R, C, a);
-	return 0;
+int main() {
+int a[R][C] = { { 1, 2, 3, 4, 5, 6 },
+                    { 7, 8, 9, 10, 11, 12 },
+                    { 13, 14, 15, 16, 17, 18 } };
+     
+    spiralPrint(R, C, a);
+    return 0;
 }
-
