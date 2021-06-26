99667b57d3d8879560eb044700ed97dcbf218f75
jtomar24 <75982877+jtomar24@users.noreply.github.com>
2021-06-26 23:44:44 +0530

    GFG

---
+++ Day 26/linked list.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+};
+
+void printLinkedList(Node* n)
+{
+    while(n!=0)
+    {
+        cout<<n->data<<" ";
+        n=n->next;
+    }
+}
+
+
+
+int main() {
+    //CREATING 3 NODES
+	Node* head=NULL;
+	Node* second=NULL;
+	Node* third=NULL;
+	
+	//ALLOCATING NODES IN HEAP
+	head=new Node();
+	second=new Node();
+	third=new Node();
+	
+	//ASSIGNING THE data
+	head->data=1;
+	head->next=second;
+	
+	second->data=2;
+	second->next=third;
+	
+	third->data=3;
+	third->next=NULL;
+	
+	printLinkedList(head);
+	
+	return 0;
+}
