1fa262d5bf8a3098fb15a6431f895a02d7b7579c
jtomar24 <75982877+jtomar24@users.noreply.github.com>
2021-06-27 23:49:10 +0530

    GFG

---
+++ Day 27/linked list implementation using structure.cpp
@@ -0,0 +1,25 @@
+struct Node
+{
+  int Data;
+  Node *next;
+  
+  //constructor
+  Node(int x)
+  {
+    data=x;
+    next=NULL;
+  }
+}
+int main()
+{
+    Node *head=new Node(10);
+    Node *second=new Node(20);
+    Node *third=new Node(30);
+  
+   head->next=second;
+   second->next=third;
+   return 0;
+  
+}
+  
+  
