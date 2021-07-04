cc134787a70352db919963abb110115556f47c3d
jtomar24 <75982877+jtomar24@users.noreply.github.com>
2021-07-04 23:51:55 +0530

    GFG

---
+++ Day 34/k largest elements.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	vector<int> kLargest(int arr[], int n, int k) {
+	    // code here
+	    sort(arr,arr+n);
+	    vector<int> v;
+	    for(int i=n-1;i>n-k-1;i--)
+	    {
+	        v.push_back(arr[i]);
+	        
+	    }
+	    return v;
+	}
+
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.kLargest(arr, n, k);
+        for (auto x : ans) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
