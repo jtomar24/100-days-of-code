a7948b96b933b12cb0ce64eedfd41980783dd86a
jtomar24 <75982877+jtomar24@users.noreply.github.com>
2021-07-13 23:05:32 +0530

    GFG

---
+++ Day 43/Matrix Interchange.cpp
@@ -0,0 +1,14 @@
+void interchange(int r, int c, int arr[][c])
+{
+    
+    // Your Code Here
+    
+    
+    // pritning elements
+    for(int i=0; i<r; i++) { 
+        for(int j=0; j<c; j++) {
+    	    printf("%d ", arr[i][j]);
+    	}
+    	printf("\n");
+    }
+}
