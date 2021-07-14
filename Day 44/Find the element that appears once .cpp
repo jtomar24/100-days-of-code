a83f94c0fc09286a9cefebea900ef99a4fc08139
jtomar24 <75982877+jtomar24@users.noreply.github.com>
2021-07-14 23:46:26 +0530

    GFG
    https://practice.geeksforgeeks.org/problems/element-appearing-once2552/1#

---
+++ Day 44/Find the element that appears once .cpp
@@ -0,0 +1,20 @@
+class Solution{
+public:	
+	int search(int A[], int N){
+	    //code
+	   
+	    for(int i=0;i<N;i=i+2)
+	    {
+	       if(A[i]!=A[i+1])
+	       {   
+	         //  break;
+	           return A[i];
+	       }
+	       else{
+	           continue;
+	       }
+	    }
+	   
+	  //  return 0;
+	}
+};
