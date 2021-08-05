649ea828584e603d75022f1c68d5e3756bffbd99
jtomar24 <75982877+jtomar24@users.noreply.github.com>
2021-08-05 23:58:26 +0530

    GFG

---
+++ Day 66/level order traversal.cpp
@@ -0,0 +1,20 @@
+void levelOrder(Node* node)
+{
+  queue<node*> q;
+  if(node==NULL)
+   return;
+  else
+  {
+   q.push(node);
+   while(!q.empty())
+   {
+    Node * curr=q.front();
+    q.pop();
+    cout<<curr->data<<" ";
+    if(curr->left!=NULL)
+     q.push(curr->left);
+    if(curr->right!=NULL)
+     q.push(curr->right);
+    }
+   }
+}
