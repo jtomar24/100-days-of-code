94197e1e173ac27e023b810e651a606ebea9b041
jtomar24 <75982877+jtomar24@users.noreply.github.com>
2021-08-12 21:53:09 +0530

    HackerRank

---
+++ Minimum Distances.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n,ans=-1,min=10000;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    for(int i=0;i<n-1;i++)
+    {
+        int key=a[i];
+        for(int k=i+1;k<n;k++)
+        {
+            if(a[k]==key)
+            {
+                ans=k-i;
+                if(ans<min)
+                {
+                    min=ans;
+                }
+            }
+        }
+    }
+    if(min==10000)
+    {
+        min=-1;
+    }
+    cout<<min;
+    return 0;
+}
